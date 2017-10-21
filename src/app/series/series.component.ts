1fea5236bf9e135f199c04bdb9d6a85efac05fd9
Or Tichon <or.tichon@gmail.com>
2017-10-21 19:14:51 +0300

    added series component

--- src/app/series/series.component.ts
+++ src/app/series/series.component.ts
@@ -1,15 +1,21 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, EventEmitter, Input, Output } from '@angular/core';
+//
+import { Series } from '../app.component';
 
 @Component({
   selector: 'app-series',
   templateUrl: './series.component.html',
   styleUrls: ['./series.component.scss']
 })
-export class SeriesComponent implements OnInit {
+export class SeriesComponent {
 
-  constructor() { }
+  @Input() series: Series[];
+  @Output() showIdChanged = new EventEmitter<number>();
 
-  ngOnInit() {
+  selectedShowId: number;
+
+  showSelected(showId: number) {
+    this.selectedShowId = showId;
+    this.showIdChanged.emit(showId);
   }
-
 }
