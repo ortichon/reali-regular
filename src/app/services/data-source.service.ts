6c333580fb48ec5f2a3058f89f7327d3cf89190d
Or Tichon <or.tichon@gmail.com>
2017-10-22 10:45:15 +0300

    Update data-source.service.ts

--- src/app/services/data-source.service.ts
+++ src/app/services/data-source.service.ts
@@ -1,7 +1,9 @@
 import { Injectable } from '@angular/core';
-import { HttpClient } from '@angular/common/http';
+import { Http, Response } from '@angular/http';
 //
 import { Observable } from 'rxjs/Observable';
+import 'rxjs/add/operator/map';
+import 'rxjs/add/operator/catch';
 //
 import { Series } from '../models/series.model';
 import { Episode } from '../models/episode.model';
@@ -12,13 +14,21 @@ export class DataSourceService {
 
   private readonly baseUrl = 'http://tvdb.reali.com/series';
 
-  constructor(private http: HttpClient) { }
+  constructor(private http: Http) { }
+
+  static handleError(error: any) {
+    return Observable.throw(new Error(error.status));
+  }
 
   getSeries(): Observable<Series[]> {
-    return this.http.get(this.baseUrl);
+    return this.http.get(this.baseUrl)
+      .map((res: Response) => res.json())
+      .catch((err: any) => DataSourceService.handleError(err));
   }
 
   getEpisodes(showId: number): Observable<Episode[]> {
-    return this.http.get(`${this.baseUrl}/${showId}`);
+    return this.http.get(${this.baseUrl}/${showId})
+      .map((res: Response) => res.json())
+      .catch((err: any) => DataSourceService.handleError(err));
   }
 }
